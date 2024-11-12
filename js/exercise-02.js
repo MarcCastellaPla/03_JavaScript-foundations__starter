d1e4c9090e145bf6458aafda1d6dc8871de52edd
MarcCastellaPla <145784203+MarcCastellaPla@users.noreply.github.com>
2024-11-12 18:34:47 +0100

    feat: complete js exercices

--- js/exercise-02.js
+++ js/exercise-02.js
@@ -5,3 +5,10 @@
     
     Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.
 */
+let anyNameILike;
+anyNameILike = "Moo!";
+console.log(anyNameILike);
+
+// const anyNameILike;
+// anyNameILike = "Moo!"; // This line will cause an error because I'm trying to reassign a value to a constant variable, which is a read-only type of variable.
+// console.log(anyNameILike);
--- js/exercise-04.js
+++ js/exercise-04.js
@@ -8,3 +8,19 @@
 
     Once you see the results, write in a comment why it works that way
 */
+
+true && console.log("moo");
+// Output: "moo"
+// This is because the && operator is a short-circuit operator, meaning it will evaluate the first operand, and since it's true, it will proceed to evaluate the second one, printing "moo" to the console.
+
+false && console.log("moo moo?")
+// No output
+// This is because the && operator is a short-circuit operator, meaning it will only evaluate the first operand, and if it's false, it won't evaluate the second one.
+
+true || console.log("hello friend");
+// No output
+// This is because the first condition is true, so it doesn't need to evaluate the second condition.
+
+false || console.log("bye friend");
+// Output: "bye friend"
+// This is because the first condition is false, so it evaluates the second condition and logs "bye friend."
